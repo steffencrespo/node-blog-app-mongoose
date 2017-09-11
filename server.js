98f6fcc8eb95821436153e7ed556c04640823c33
Leonardo Steffen <leosteffen84@gmail.com>
2017-09-11 18:12:40 -0400

    adds runServer method to allow for testing

--- server.js
+++ server.js
@@ -33,3 +33,28 @@ app.put('/posts/:id', (req, res) => {
 app.delete('/posts/:id', (req, res) => {
 
 });
+
+let server;
+
+function runServer(databaseUrl=DATABASE_URL, port=PORT){
+	return new Promise((resolve, reject) => {
+		mongoose.connect(databaseUrl, err => {
+			if(err) {
+				return reject(err);
+			}
+
+			server = app.listen(port, () => {
+				console.log(`Your app is listening on port ${port}`);
+				resolve();
+			})
+			.on('error', err => {
+				mongoose.disconnect();
+				reject(err);
+			})
+		})
+	})
+}
+
+if (require.main === module) {
+	runServer().catch(err => console.error(err));
+}
