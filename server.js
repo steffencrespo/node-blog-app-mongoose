de3a09a1b0c2fbe7221a5283a01b7edb5b3ee3be
Leonardo Steffen <leosteffen84@gmail.com>
2017-09-12 21:08:00 -0400

    adds put endpoint

--- server.js
+++ server.js
@@ -68,7 +68,25 @@ app.post('/posts', (req, res) => {
 });
 
 app.put('/posts/:id', (req, res) => {
+	if(!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+		const message = (`Request path id ${req.params.id} and request body id ${req.body.id} must match`);
+		console.error(message);
+		return res.status(400).json({message: message});
+	}
 
+	const toUpdate = {};
+	const updateableFields = ['title', 'content', 'firstName', 'lastName'];
+
+	updateableFields.forEach(field => {
+		if(field in req.body) {
+			toUpdate[field] = req.body[field];
+		}
+	});
+
+	Post
+	.findByIdAndUpdate(req.body.id, {$set: toUpdate})
+	.then(post => res.status(204).end())
+	.catch(err => res.status(500).json({message: 'Internal Server Error'}));
 });
 
 app.delete('/posts/:id', (req, res) => {
